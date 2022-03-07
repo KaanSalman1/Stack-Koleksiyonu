18c14e5c52542376277a9cc4d3a9cbacb3312f3f
KaanSalman1 <100555439+KaanSalman1@users.noreply.github.com>
2022-03-07 10:08:40 +0300

    Stack Koleksiyon Uygulaması Kodlarım

---
+++ Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace StackUygulama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private Stack yigin = new Stack(); 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            yigin.Push(textBox1.Text);
+            Listele();
+        }
+
+        private void Listele()
+        {
+            listBox1.Items.Clear();
+            foreach (string eleman in yigin)
+            {
+                listBox1.Items.Add(eleman);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            yigin.Pop();
+            Listele();
+        }
+    }
+}
